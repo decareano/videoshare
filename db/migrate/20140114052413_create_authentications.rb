5d29db20f1501058f3c266c7baa9c00876bfcff2
marcelo <almagro@comcast.net>
2014-01-22 21:03:49 -0800

    all files untracked and modified ones

--- db/migrate/20140114052413_create_authentications.rb
+++ db/migrate/20140114052413_create_authentications.rb
@@ -3,7 +3,7 @@ class CreateAuthentications < ActiveRecord::Migration
     create_table :authentications do |t|
       t.integer :user_id
       t.string :provider
-      t.stringindex :uid
+      t.string :uid
       t.string :create
       t.string :destroy
 
---
+++ spec/factories/authentications.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :authentication do
+    user_id 1
+    provider "MyString"
+    uid ""
+    create "MyString"
+    destroy "MyString"
+  end
+end
